a1abf52607275bb68a8b646009ce4926a9643d46
DaSch <dasch@daschmedia.de>
2014-11-08 00:34:46 +0100

    new version 0.6.2

--- FormatNum.php
+++ FormatNum.php
@@ -26,7 +26,7 @@ $wgExtensionCredits['parserhook'][] = array (
 	'url'=>'http://www.mediawiki.org/wiki/Extension:FormatNum',
 	'descriptionmsg' => 'formatnum-desc',
 	'author'=>'[http://www.dasch-tour.de DaSch]',
-	'version'=>'0.6.1',
+	'version'=>'0.6.2',
 );
 $dir = __DIR__.'/';
 
